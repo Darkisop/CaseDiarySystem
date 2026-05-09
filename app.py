3e88c6e8a942d74da48bb8750059cf62eb660a82
L Chinmaya Sahu <lchinmayasahu311@gmail.com>
2026-05-09 23:00:09 +0530

    Removed police term

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1>Police Case Diary System</h1>
+    <h1>Case Diary System</h1>
     <h2>Deployment Successful</h2>
     <p>Your Flask application is now live on Render.</p>
     """
